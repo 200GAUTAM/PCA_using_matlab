b146da1b4d4091fb50411faac583e119ccb8b4b0
mittal <200mittalgautam@gmail.com>
2017-03-03 22:18:24 +0530

    last commit

--- main.m
+++ main.m
@@ -11,3 +11,17 @@ pause;
 
 [X_norm, mu, sigma] = normalize(X);
 
+[U, S] = pca(X_norm);
+
+display(U);
+hold on;
+hold on;
+drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
+drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
+hold off;
+
+fprintf('Top eigenvector: \n');
+fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
+fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
+
+
