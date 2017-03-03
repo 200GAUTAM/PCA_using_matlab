223ed5b7276b5744e92b366400891e357e70ed51
mittal <200mittalgautam@gmail.com>
2017-03-03 19:29:22 +0530

    changes

--- main.m
+++ main.m
@@ -2,8 +2,12 @@ clear;
 close all;
 clc;
 
-fprintf('visualize 2d data set');
+fprintf('visualize 2d data set\n');
 load('data1.mat');
 plot(X(:, 1), X(:, 2), 'bo');
 axis([0.5 6.5 2 8]); axis square;
+fprintf('Print any key to continue.....\n');
 pause;
+
+[X_norm, mu, sigma] = normalize(X);
+
